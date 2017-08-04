8f6fef9bfd4b042841ccdd2b5cacfd5b3be112bf
Janak Shrestha <old_ink@hotmail.com>
2017-08-04 11:58:07 +0545

    Removed auth layer in fb

--- Lumle.AuthServer/Infrastructures/GrantTypes/FacebookAuthValidator.cs
+++ Lumle.AuthServer/Infrastructures/GrantTypes/FacebookAuthValidator.cs
@@ -2,6 +2,7 @@
 using System.Net.Http;
 using System.Threading.Tasks;
 using IdentityModel;
+using IdentityServer4.Models;
 using IdentityServer4.Validation;
 using Lumle.AuthServer.Data.Entities;
 using Lumle.AuthServer.Data.Store;
@@ -27,110 +28,102 @@ namespace Lumle.AuthServer.Infrastructures.GrantTypes
 
         public async Task ValidateAsync(ExtensionGrantValidationContext context)
         {
-            var inputToken = context.Request.Raw.Get("input_token");
-            var accessToken = context.Request.Raw.Get("access_token");
-
-            if (string.IsNullOrEmpty(inputToken))
+            try
             {
-                context.Result = new GrantValidationResult(OidcConstants.TokenErrors.InvalidRequest, null);
-                return;
-            }
+                var inputToken = context.Request.Raw.Get("input_token");
+                var accessToken = context.Request.Raw.Get("access_token");
 
-            if (string.IsNullOrEmpty(accessToken))
-            {
-                context.Result = new GrantValidationResult(OidcConstants.TokenErrors.InvalidRequest, null);
-                return;
-            }
-            
-            var client = new HttpClient();
-            var tokenResponse = await client.GetAsync($"https://graph.facebook.com/debug_token?input_token={inputToken}&access_token={accessToken}");
-
-            if (tokenResponse.StatusCode == System.Net.HttpStatusCode.OK)
-            {
-                //Get token response and check wheather ist from valid application or not
-                var tokenResponseData = await tokenResponse.Content.ReadAsStringAsync();
-                var fbTokenResponseData = TokenResponseDserializer.DserializeIdToken<FacebookAuthTokenResponse>(tokenResponseData);
-
-                //if (fbTokenResponseData.Data.App_id != FacebookAuthConstants.FacebookAppId)
-                //{
-                //    context.Result = new GrantValidationResult(OidcConstants.TokenErrors.UnauthorizedClient, null);
-                //    return;
-                //}
-
-                //Get user profile information using access_token from Facebook Graph API
-                var profileResponse = await client.GetAsync($"https://graph.facebook.com/me?fields=id,email,gender,picture&access_token={inputToken}");
-
-                if (profileResponse.IsSuccessStatusCode)
+                if (string.IsNullOrEmpty(inputToken))
                 {
-                    //Parse fb response
-                    var responseData = await profileResponse.Content.ReadAsStringAsync();
-                    var fbTokenResponse = TokenResponseDserializer.DserializeIdToken<FacebookAuthResponse>(responseData);
-
-
-                    //Check if user is available in Auth Database yet.
-                    var requestedUser = _userStore.FindByProviderAndSubjectId(ProviderConstants.Facebook, fbTokenResponse.Id);
-
-
-                    if (requestedUser != null && requestedUser.IsBlocked)
-                    {
-                        context.Result = new GrantValidationResult("Requested User is Blocked.", null);
-                        return;
-                    }
-
-                    if (requestedUser == null)
-                    {
-                        //Check if user with this email is already exist or not [Does not depend on provider]
-                        if (!string.IsNullOrEmpty(fbTokenResponse.Email))
-                        {
-                            if (_userStore.IsEmailExist(fbTokenResponse.Email.ToLower()))
-                            {
-                                context.Result = new GrantValidationResult("Error. An account has been already created with this email.", null);
-                                return;
-                            }
-                        }
-                        
-
-                        #region Password Hash and Salt
-
-                        var pwdSalt = CryptoService.GenerateSalt();
-                        var pwdHash = CryptoService.ComputeHash(PasswordGenerator.GetRandomPassword(), pwdSalt);
-                        #endregion
-
-                        //Get User Information
-
-                        //Create new User
-                        var customUser = new MobileUser
-                        {
-                            SubjectId = fbTokenResponse.Id,
-                            Email = EmailValidator.IsValidEmail(fbTokenResponse.Email)? fbTokenResponse.Email.ToLower():"",
-                            UserName = EmailValidator.IsValidEmail(fbTokenResponse.Email) ? fbTokenResponse.Email.ToLower() : "",
-                            PasswordHash = Convert.ToBase64String(pwdHash),
-                            PasswordSalt = Convert.ToBase64String(pwdSalt),
-                            PhoneNo = "",
-                            FirstName = "",
-                            LastName = "",
-                            IsStaff = false,
-                            ProfileUrl = fbTokenResponse.Picture.Data.Url,
-                            Gender = GenderResolver.GetNumericGenderValue(fbTokenResponse.Gender),
-                            IsEmailVerified = true, //Verified by default
-                            IsBlocked = false,
-                            Provider = ProviderConstants.Facebook,
-                            CreatedDate = DateTime.UtcNow,
-                            LastUpdated = DateTime.UtcNow
-
-                        };
-
-                        _userStore.AddNewUser(customUser);
-                        //_userDbContext.SaveChanges();
-                    }
-
-                    context.Result = new GrantValidationResult(fbTokenResponse.Id, ExternalGrantTypes.Facebok);
-                }
-                else
                     context.Result = new GrantValidationResult(OidcConstants.TokenErrors.InvalidRequest, null);
+                    return;
+                }
+
+                if (string.IsNullOrEmpty(accessToken))
+                {
+                    context.Result = new GrantValidationResult(OidcConstants.TokenErrors.InvalidRequest, null);
+                    return;
+                }
+
+                using (var client = new HttpClient())
+                {
+                    var profileResponse = await client.GetAsync($"https://graph.facebook.com/me?fields=id,email,gender,picture&access_token={accessToken}");
+
+                    if (profileResponse.IsSuccessStatusCode)
+                    {
+                        //Parse fb response
+                        var responseData = await profileResponse.Content.ReadAsStringAsync();
+                        var fbTokenResponse = TokenResponseDserializer.DserializeIdToken<FacebookAuthResponse>(responseData);
+
+
+                        //Check if user is available in Auth Database yet.
+                        var requestedUser = _userStore.FindByProviderAndSubjectId(ProviderConstants.Facebook, fbTokenResponse.Id);
+
+
+                        if (requestedUser != null && requestedUser.IsBlocked)
+                        {
+                            context.Result = new GrantValidationResult(TokenRequestErrors.InvalidRequest, "user is blocked.");
+                            return;
+                        }
+
+                        if (requestedUser == null)
+                        {
+                            //Check if user with this email is already exist or not [Does not depend on provider]
+                            if (!string.IsNullOrEmpty(fbTokenResponse.Email))
+                            {
+                                if (_userStore.IsEmailExist(fbTokenResponse.Email.ToLower()))
+                                {
+                                    context.Result = new GrantValidationResult(TokenRequestErrors.InvalidRequest, "User with this email already exist.");
+                                    return;
+                                }
+                            }
+
+
+                            #region Password Hash and Salt
+
+                            var pwdSalt = CryptoService.GenerateSalt();
+                            var pwdHash = CryptoService.ComputeHash(PasswordGenerator.GetRandomPassword(), pwdSalt);
+                            #endregion
+
+                            //Get User Information
+
+                            //Create new User
+                            var customUser = new MobileUser
+                            {
+                                SubjectId = fbTokenResponse.Id,
+                                Email = EmailValidator.IsValidEmail(fbTokenResponse.Email) ? fbTokenResponse.Email.ToLower() : "",
+                                UserName = EmailValidator.IsValidEmail(fbTokenResponse.Email) ? fbTokenResponse.Email.ToLower() : "",
+                                PasswordHash = Convert.ToBase64String(pwdHash),
+                                PasswordSalt = Convert.ToBase64String(pwdSalt),
+                                PhoneNo = "",
+                                FirstName = string.Empty,
+                                LastName = string.Empty,
+                                IsStaff = false,
+                                ProfileUrl = fbTokenResponse.Picture.Data.Url,
+                                Gender = GenderResolver.GetNumericGenderValue(fbTokenResponse.Gender),
+                                IsEmailVerified = true, //Verified by default
+                                IsBlocked = false,
+                                Provider = ProviderConstants.Facebook,
+                                CreatedDate = DateTime.UtcNow,
+                                LastUpdated = DateTime.UtcNow
+
+                            };
+
+                            _userStore.AddNewUser(customUser);
+                            //_userDbContext.SaveChanges();
+                        }
+
+                        context.Result = new GrantValidationResult(fbTokenResponse.Id, ExternalGrantTypes.Facebok);
+                    }
+                    else
+                        context.Result = new GrantValidationResult(TokenRequestErrors.InvalidRequest, "unable to access user profile.");
+                }
+
             }
-            else
-                context.Result = new GrantValidationResult(OidcConstants.TokenErrors.InvalidRequest, null);
+            catch (Exception)
+            {
+                context.Result = new GrantValidationResult(TokenRequestErrors.InvalidRequest, "Internal Server Error.");
+            }
         }
     }
 }
