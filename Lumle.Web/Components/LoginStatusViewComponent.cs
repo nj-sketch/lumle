5e2e81a6b7a4950abc997e1d72953917e3114674
Niraj Lama <niraj@ekbana.com>
2017-10-26 16:52:18 +0545

    Replaced _LoginPartial with LoginStatusViewComponent

---
+++ Lumle.Web/Components/LoginStatusViewComponent.cs
@@ -0,0 +1,32 @@
+﻿using Lumle.Data.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace Lumle.Web.Components
+{
+    public class LoginStatusViewComponent : ViewComponent
+    {
+        private readonly SignInManager<User> _signInManager;
+        private readonly UserManager<User> _userManager;
+
+        public LoginStatusViewComponent(SignInManager<User> signInManager, UserManager<User> userManager)
+        {
+            _signInManager = signInManager;
+            _userManager = userManager;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            if (_signInManager.IsSignedIn(HttpContext.User))
+            {
+                var user = await _userManager.GetUserAsync(HttpContext.User);
+                return View("LoggedIn", user);
+            }
+            else
+            {
+                return View();
+            }
+        }
+    }
+}
